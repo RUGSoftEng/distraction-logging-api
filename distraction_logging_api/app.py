1566247aa3317080fb777e36fb1059ecb3bcb958
Daffurn <alexlewis-@hotmail.co.uk>
2017-06-12 14:13:58 +0200

    fixed app.py

--- distraction_logging_api/app.py
+++ distraction_logging_api/app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
-from .database import session_scope, Log
+from database import session_scope, Log
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
+# Query to get size of the database, this is used as a primary key
+with session_scope() as db_session:
+    dbSize = db_session.query(Log).count() + 1
+   
 
 # show all data
 @app.route('/')
@@ -11,16 +17,20 @@ def index():
         # when using the object retrieved from the database outside
         # the with-scope, use the line below to extract the data.
         db_session.expunge_all()
-
     return render_template('index.html', data=data)
 
 
+""" Receives a list of JSON objects, with one JSON object corresponding to one log (change in options, a redirect etc).
+We then iterate through these objects and enter them into the database. Each user sends a list of their logs once a day """ 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
+    global dbSize
     data = request.get_json()
-    entry = Log(id=data['id'], event=data['event'], value=data['value'], time=data['time'])
-    with session_scope() as db_session:
-        db_session.add(entry)
+    for i in data:
+        entry = Log(id=dbSize, userID=i['id'], event=i['event'], trigger=i['trigger'], value=i['value'], time=i['time'], type=i['type'])
+        with session_scope() as db_session:
+            db_session.add(entry)
+        dbSize += 1
     return "ok"
 
 
